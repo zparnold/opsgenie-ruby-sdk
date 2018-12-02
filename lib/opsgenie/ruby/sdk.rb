ec3499f5618ccbeba050904be5e7a78eebcf441f
Zach Arnold <me@zacharnold.org>
2018-12-01 16:14:42 -0800

    init gem

---
+++ lib/opsgenie/ruby/sdk.rb
@@ -0,0 +1,9 @@
+require "opsgenie/ruby/sdk/version"
+
+module Opsgenie
+  module Ruby
+    module Sdk
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/opsgenie/ruby/sdk_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Opsgenie::Ruby::Sdk do
+  it "has a version number" do
+    expect(Opsgenie::Ruby::Sdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
